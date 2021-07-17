fa4d365f45a8986cd4c8e33b7236a50626f62e14
praveenkumar021 <pravee.nku04@gmail.com>
2021-07-17 10:18:13 +0530

    commit Massage

---
+++ src/org/Test/Sample2.java
@@ -0,0 +1,11 @@
+package org.Test;
+
+public class Sample2 {
+	public static void main(String[] args) {
+		
+		System.out.println("done1");
+		System.out.println("done2");
+		System.out.println("done3");
+	}
+
+}
